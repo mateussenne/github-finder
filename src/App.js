8da6346ae23147ce22ab8bb17a5c25aaedb8214e
Mateus Senne <mateussenne@gmail.com>
2021-07-06 00:31:09 -0300

    Add about page and alert component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +12,8 @@ import './App.css';
 class App extends Component{
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
   
   // async componentDidMount(){
@@ -31,19 +35,35 @@ class App extends Component{
     });
   }
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+    setTimeout(() => this.setState({alert: null}), 5000)
+  }
+
   // Clear users from state
   clearUsers = () => this.setState({users: [], loading: false });
 
   render(){    
     const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClearButton={users.length > 0 ? true : false}/>
-          <Users loading={loading} users={users}/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClearButton={users.length > 0 ? true : false} setAlert={this.setAlert}/>
+                  <Users loading={loading} users={users}/>
+                </Fragment>
+              )}/>
+              <Route exact path="/about" component={About}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
